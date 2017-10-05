fa182401079eb83a8ccdc025ab79e84ede65a345
ivan <ivan@localhost.com>
2017-10-05 14:07:22 +0300

    cleanup code

--- login.php
+++ login.php
@@ -9,23 +9,7 @@ $table = "users";
 /* Создаем соединение */
 $link = mysqli_connect($hostname, $username, $password) or die ("Не могу создать соединение");
 mysqli_select_db($link, $dbName) or die (mysqli_error($link));
-
-switch ($_GET['action']) {
-    case get:
-
-        $query = "SELECT * FROM $table";
-        $res = mysqli_query($link, $query) or die(mysqli_error($link));
-        $i = 0;
-        while ($row = mysqli_fetch_array($res)) {
-            echo '<tr class="res' . $i . '" style="border: solid 1px #000"> <td value="'. $row['Переменная'] .'"id="vare' . $i .'" name="vare' . $i . '">' . $row['Переменная'] . '</td> <td><input id="vale' . $i . '" name="vale' . $i . '" value="' . $row['Значение'] . '" type="text"></td></tr>';
-            $i++;
-        }
-
-        break;
-
-    case post:
-
-         $login_username =  $_POST['username'];
+ $login_username =  $_POST['username'];
          $login_password = $_POST['password'];
          $query = "select Id from ".$table." where username='".$login_username."' and pass='".$login_password."';";
           $res = mysqli_query($link, $query) or die(mysqli_error($link));
@@ -38,9 +22,7 @@ switch ($_GET['action']) {
        mysqli_query($link, $query) or die(mysqli_error($link));
        echo 2;
        }
-        break;
 
-}
 
 mysqli_close($link);
 ?>
