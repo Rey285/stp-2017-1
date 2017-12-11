253046dab73b40543eb79738301d58ce42c01c19
ivan <ivan@localhost.com>
2017-12-12 00:46:21 +0300

    refactor

--- upload.php
+++ upload.php
@@ -1,32 +1,32 @@
 <?php
 
-// Здесь нужно сделать все проверки передаваемых файлов и вывести ошибки если нужно
+    // Здесь нужно сделать все проверки передаваемых файлов и вывести ошибки если нужно
 
-// Переменная ответа
+    // Переменная ответа
 
-$data = array();
+    $data = array();
 
-if( isset( $_GET['uploadfiles'] ) ){
-    $error = false;
-    $files = array();
+    if( isset( $_GET['uploadfiles'] ) ){
+        $error = false;
+        $files = array();
 
-    $uploaddir = './music/'; // . - текущая папка
+        $uploaddir = './music/'; // . - текущая папка
 
-    // Создадим папку если её нет
+        // Создадим папку если её нет
 
-    if( ! is_dir( $uploaddir ) ) mkdir( $uploaddir, 0777 );
+        if( ! is_dir( $uploaddir ) ) mkdir( $uploaddir, 0777 );
 
-    // переместим файлы из временной директории в указанную
-    foreach( $_FILES as $file ){
-        if( move_uploaded_file( $file['tmp_name'], $uploaddir . basename($file['name']) ) ){
-            $files[] = realpath( $uploaddir . $file['name'] );
+        // переместим файлы из временной директории в указанную
+        foreach( $_FILES as $file ){
+            if( move_uploaded_file( $file['tmp_name'], $uploaddir . basename($file['name']) ) ){
+                $files[] = realpath( $uploaddir . $file['name'] );
+            }
+            else{
+                $error = true;
+            }
         }
-        else{
-            $error = true;
-        }
+
+        $data = $error ? array('error' => 'Ошибка загрузки файлов.') : array('files' => $files );
+
+        echo json_encode( $data );
     }
-
-    $data = $error ? array('error' => 'Ошибка загрузки файлов.') : array('files' => $files );
-
-    echo json_encode( $data );
-}
